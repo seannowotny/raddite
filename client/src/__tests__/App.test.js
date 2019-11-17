c92d64517050d4e1701dad961e162dabfafb72f1
seannowotny <seannowotny@live.de>
2019-11-17 17:12:50 +0900

    Move App component test to __tests__ folder

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from '../App';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => 
+{
+  const counter = renderer.create(<App />).toJSON();
+  expect(counter).toMatchSnapshot();
+});
