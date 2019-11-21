cb0dace2f97ca83832520ef451cfc6347938b683
SNx42 <seannowotny@live.de>
2019-11-21 15:39:28 +0900

    Install selenium-webdriver, mocha and write first e2e test

---
+++ client/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+   rootUrl: "http://localhost:3000",
+   localAdmin: {
+      email: "jdoe@gmail.com",
+      password: "12345678"
+   },
+   e2e: {
+      drivers: ['firefox']
+   }
+};
